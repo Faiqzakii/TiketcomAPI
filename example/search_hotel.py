66e6205b1d0e635d4a9b85bcaa907c90cd92917a
Faiq Zakki <221709674@stis.ac.id>
2020-10-12 21:28:08 +0000

    Adding some changes

--- example/search_hotel.py
+++ example/search_hotel.py
@@ -14,16 +14,44 @@ if __name__ == '__main__':
 
     api = Client()
 
-    result = api.search()
-    
+    tanggal = input('Scraping data tanggal berapa? (Format YYYY-MM-DD, contoh: 2020-10-13) : ')
+
+    while True:
+        prov = input('Scraping data provinsi mana? (Bali, Yogyakarta, NTB, Sulsel, Sumut) : ')
+        if prov.lower() == 'bali':
+            region = 'REGION'
+            city = 'bali-108001534490276212'
+            break
+        elif prov.lower() == 'yogyakarta':
+            region = 'REGION'
+            city = 'yogyakarta-province-108001534490276304'
+            break
+        elif prov.lower() == 'ntb':
+            region = 'REGION'
+            city = 'west-nusa-tenggara-108001534490280165'
+            break
+        elif prov.lower() == 'sulsel':
+            region = 'REGION'
+            city = 'south-sulawesi-108001534490277179'
+            break
+        elif prov.lower() == 'sumut':
+            region = 'REGION'
+            city = 'north-sumatera-108001534490276409'
+            break
+        else:
+            print('Kode kota tidak ditemukan')
+
+
+    result = api.search(region, city, tanggal, page = 2)
+
     _dir = os.path.dirname(__file__)
     _dir = os.path.join(_dir, 'json')
-    
+
     if not os.path.exists(_dir):
         print("\n\nCreating json folder..\n\n")
         os.makedirs(_dir)
-    
-    with open(_dir + '/search.json', 'w') as outfile:
+
+    with open(_dir + '/{}_{}.json'.format(prov, tanggal), 'w') as outfile:
         json.dump(result, outfile, indent=2)
 
     print('All ok')
---
+++ readme.md
@@ -0,0 +1,11 @@
+# TokopediaAPI
+
+A Python wrapper for the Tiket.com private API for hotel search.
+
+## Support
+
+Make sure to review the [contributing documentation](CONTRIBUTING.md) before submitting an issue report or pull request.
+
+## Legal
+
+Disclaimer: This is not affliated, endorsed or certified by Tiket.com. This is an independent and unofficial API. Strictly **not for spam**. Use at your own risk.
