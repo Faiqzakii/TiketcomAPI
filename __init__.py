3f350be2f74900b5e43eb659ef651ee29f1f82a2
Faiq Zakki <37260969+Faiqzakii@users.noreply.github.com>
2020-12-15 09:34:11 +0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,6 +1,6 @@
 from .client import Client
 
-__copyright__ = 'Copyright 2020 by Kevin Jonathan'
+__copyright__ = 'Copyright 2020 by Faiq Zakki'
 __version__ = '1.1.0'
 __author__ = 'Faiq Zakki'
 __author_email__ = '221709674@stis.ac.id'
